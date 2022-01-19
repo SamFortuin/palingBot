0e14aee2d98e7adab6f9190665b7cbb6b175de40
SamFortuin <99059050@mydavinci.nl>
2022-01-19 12:11:10 +0100

    added grabToken func

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 from shortcuts import *
+import discord
+from discord.ext import commands
+from discord.errors import ClientException
+
+def grabToken():
+    #grabs token from the txt containing it
+    f = open('token.txt','r')
+    tokenOut = f.read()
+    print('succesfully grabbed token') if tokenOut != None else print('failed to get token')
+    f.close()
+    return tokenOut
